597841cf6035760c80c81842dae29a940833e51a
cwon.lee <wonmyth@naver.com>
2024-07-03 15:33:30 +0900

    (feature) step 1~4

--- baseball/baseball.cpp
+++ baseball/baseball.cpp
@@ -1,3 +1,47 @@
-class Basball {
+#include <stdexcept>
+using namespace std;
 
+struct GuessResult {
+	bool solved;
+	int strikes;
+	int balls;
+};
+class Baseball {
+public:
+	explicit Baseball(const string& question)
+		:question(question) {
+	}
+	GuessResult guess(const string& guessNumber) {
+		assertIllegalArgument(guessNumber);
+
+		if (guessNumber == question) {
+			return { true, 3, 0 };
+		}
+		return { false, 0, 0 };
+	}
+
+	void assertIllegalArgument(const std::string& guessNumber)
+	{
+		if (guessNumber.length() != 3) {
+			throw length_error("Must be three letters.");
+		}
+
+		for (char ch : guessNumber) {
+			if (ch >= '0' && ch <= '9') continue;
+			throw invalid_argument("Must be number");
+
+		}
+
+		if (isDuplicatedNumber(guessNumber)) {
+			throw invalid_argument("Must not have the same number");
+		}
+	}
+
+	bool isDuplicatedNumber(const string& guessNumber) {
+		return (guessNumber[0] == guessNumber[1] ||
+			guessNumber[1] == guessNumber[2] ||
+			guessNumber[2] == guessNumber[0]);
+	}
+private:
+	string question;
 };
--- baseball_test/test.cpp
+++ baseball_test/test.cpp
@@ -1,7 +1,32 @@
 #include "pch.h"
 #include "../baseball/baseball.cpp"
+#include <iostream>
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace std;
+
+class BaseballFixture : public testing::Test {
+public:
+	Baseball game{ "123" };
+	void assertIllegalArgument(string guessNumber) {
+		try {
+			game.guess(guessNumber);
+			FAIL();
+		}
+		catch (exception e) {
+			// PASS
+		}
+	}
+};
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
+	assertIllegalArgument("12");
+	assertIllegalArgument("12s");
+	assertIllegalArgument("121");
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber) {
+	GuessResult result = game.guess("123");
+
+	EXPECT_TRUE(result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
